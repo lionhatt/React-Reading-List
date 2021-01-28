c943c608675ca3c60a1b779b211d167cd601202d
Lionhatt <gavindxy@gmail.com>
2021-01-28 21:22:21 +1100

    udate server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
+
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -16,8 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-const url = process.env.MONGODB_URI;
-mongoose.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
 // Start the API server
 app.listen(PORT, function() {

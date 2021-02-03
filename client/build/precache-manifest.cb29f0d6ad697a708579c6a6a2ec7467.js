5068105fe7bfc0f1e64dd8c27cb2d402603abe9d
Lionhatt <gavindxy@gmail.com>
2021-02-03 21:15:51 +1100

    ready for deployment

---
+++ client/build/precache-manifest.cb29f0d6ad697a708579c6a6a2ec7467.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a515a02a4cc60bdd1c98",
+    "url": "/static/css/main.69969a11.chunk.css"
+  },
+  {
+    "revision": "a515a02a4cc60bdd1c98",
+    "url": "/static/js/main.95553a5b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "87f0fa050518d3dcb537",
+    "url": "/static/js/2.0c1e278c.chunk.js"
+  },
+  {
+    "revision": "7ab97fbb4e6d312e93e1d86b04de8880",
+    "url": "/index.html"
+  }
+];
